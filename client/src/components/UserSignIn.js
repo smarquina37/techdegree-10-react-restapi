6c4d5e132e06796fce1ef18530745d5332458719
smarquina37 <56809615+smarquina37@users.noreply.github.com>
2022-11-23 19:37:04 -0800

    Update

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,32 +1,41 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 export const UserSignIn = ({ context }) => {
-  // let navigate = useNavigate();
-  const [user, getUser] = useState({
+  const [user, setUser] = useState({
     emailAddress: "",
     password: "",
+    errors: [],
   });
 
-  const { emailAddress, password } = user;
-  // const [emailAdress, setEmailAddress] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [errors, setErrors] = useState([]);
+  const navigate = useNavigate();
 
   // const user = {
-  //   emailAdress,
+  //   emailAddress,
   //   password,
   // };
 
-  const handleChange = (e) => {
-    getUser({ ...user, [e.target.name]: [e.target.value] });
+  const handleSubmit = (e) => {
+    context.data.actions
+      .signIn(username, password)
+      .then((user) => {
+        if (user === null) {
+          setErrors(() => {
+            return { errors: ["Sign-in was unsuccessful"] };
+          });
+        } else {
+          navigate("/authenticated");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        navigate("/error");
+      });
   };
 
-  const handleSubmit = (e) => {
+  const handleChange = (e) => {
     e.prevantDefault();
-    console.log(user);
-    // navigate("/");
-    // e.currentTarget.reset();
+    setUser({ ...user, [e.target.name]: [e.target.value] });
   };
 
   return (
@@ -52,7 +61,9 @@ export const UserSignIn = ({ context }) => {
         <button className="button" type="submit">
           Sign In
         </button>
-        <button className="button button-secondary">Cancel</button>
+        <button className="button button-secondary" onClick={handleCancel}>
+          Cancel
+        </button>
       </form>
       <p>
         Don't have a user account? Click here to{" "}
@@ -63,3 +74,25 @@ export const UserSignIn = ({ context }) => {
 };
 
 export default UserSignIn;
+// const [emailAddress, setEmailAddress] = useState("");
+// const [password, setPassword] = useState("");
+// const [error, setError] = useState(null);
+// const [loading, setLoading] = useState(false);
+
+// useEffect(() => {
+//   context.data.getUser(user)
+//   .then(user =>{
+//     if (user === null) {
+//       createUser(() => {
+//         return {errors: ['sign-in was unsuccessful']}
+//       })
+//     } else {
+//           navigate('/authenticated');
+//           console.log(`SUCCESS! ${username} is now signed in!`)
+//         }
+//       .catch(err => {
+//         console.log(err)
+//       })
+//     })
+//   // eslint-disable-next-line react-hooks/exhaustive-deps
+// }, []);
