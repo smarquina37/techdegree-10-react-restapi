270d6440cd7fc59465b45aa3a9cd3a975792e900
smarquina37 <56809615+smarquina37@users.noreply.github.com>
2022-11-25 10:42:55 -0800

    Update UserSignUp

--- client/src/components/UserSignIn.js
+++ client/src/components/UserSignIn.js
@@ -1,69 +1,31 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 export const UserSignIn = ({ context }) => {
-  const [user, setUser] = useState({
-    emailAddress: "",
-    password: "",
-    errors: [],
-  });
-
-  const navigate = useNavigate();
-
-  const user = {
-    emailAddress,
-    password,
-  };
-
-  const handleSubmit = (e) => {
-    context.data.actions
-      .signIn(username, password)
-      .then((user) => {
-        if (user === null) {
-          setErrors(() => {
-            return { errors: ["Sign-in was unsuccessful"] };
-          });
-        } else {
-          navigate("/authenticated");
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-        navigate("/error");
-      });
-  };
-
-  const handleChange = (e) => {
-    e.prevantDefault();
-    setUser({ ...user, [e.target.name]: [e.target.value] });
-  };
-
   return (
     <div className="form--centered">
       <h2>Sign In</h2>
-      <form onSubmit={handleSubmit}>
+      <form>
         <label htmlFor="emailAddress">Email Address</label>
         <input
           id="emailAddress"
           name="emailAddress"
           type="email"
-          onChange={handleChange}
-          value={emailAddress}
+          // onChange={handleChange}
+          value=""
         />
         <label htmlFor="password">Password</label>
         <input
           id="password"
           name="password"
           type="password"
-          onChange={handleChange}
-          value={password}
+          // onChange={handleChange}
+          value=""
         />
         <button className="button" type="submit">
           Sign In
         </button>
-        <button className="button button-secondary" onClick={handleCancel}>
-          Cancel
-        </button>
+        <button className="button button-secondary">Cancel</button>
       </form>
       <p>
         Don't have a user account? Click here to{" "}
--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 const UserSignUp = ({ context }) => {
-  let navigate = useNavigate();
   const [firstName, setFirstName] = useState("");
-  const [lastName, setlastName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [emailAddress, setEmailAddress] = useState("");
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState([]);
@@ -16,26 +15,29 @@ const UserSignUp = ({ context }) => {
     password,
   };
 
-  //   const handleChange = (e) => {
-  //     e.preventDefault();
-  //     if (e.target.name === "firstName") {
-  //       setFirstName(value);
-  //     } else if (e.target.name === "lastName") {
-  //       setlastName(value);
-  //     } else if (e.target.name === "emailAddress") {
-  //       setEmailAddress(value);
-  //     } else {
-  //       return;
-  //     }
-  //   };
-  // };
+  let navigate = useNavigate();
+
+  const handleChange = (e) => {
+    e.prevantDefault();
+    if (e.target.name === "firstName") {
+      setFirstName(value);
+    } else if (e.target.name === "lastName") {
+      setLastName(value);
+    } else if (e.target.name === "emailAddress") {
+      setEmailAddress(value);
+    } else {
+      return;
+    }
+  };
 
   const handleSubmit = (e) => {
+    e.prevantDefault();
+
     context.data
       .createUser(user)
       .then((errors) => {
         if (errors.length) {
-          setErrors({ errors });
+          setErrors(errors);
         } else {
           context.actions.signIn(emailAddress, password).then(() => {
             navigate("/authenticated");
@@ -46,63 +48,61 @@ const UserSignUp = ({ context }) => {
         console.log(err);
         navigate("/error");
       });
-
-    const handleCancel = (e) => {
-      e.preventDefault();
-      navigate("/");
-    };
-
-    return (
-      <div class="form--centered">
-        <h2>Sign Up</h2>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="firstName">First Name</label>
-          <input
-            id="firstName"
-            name="firstName"
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          <label htmlFor="lastName">Last Name</label>
-          <input
-            id="lastName"
-            name="lastName"
-            type="text"
-            value={lastName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          <label htmlFor="emailAddress">Email Address</label>
-          <input
-            id="emailAddress"
-            name="emailAddress"
-            type="email"
-            value={emailAddress}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-          <label htmlFor="password">Password</label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <button className="button" type="submit">
-            Sign Up
-          </button>
-          <button className="button button-secondary" onclick={handleCancel}>
-            Cancel
-          </button>
-        </form>
-        <p>
-          Already have a user account? Click here to{" "}
-          <Link to="sign-in.html">sign in</Link>!
-        </p>
-      </div>
-    );
   };
+
+  return (
+    <div class="form--centered">
+      <h2>Sign Up</h2>
+
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name</label>
+        <input
+          id="firstName"
+          name="firstName"
+          type="text"
+          value={firstName}
+          onChange={handleChange}
+        />
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          id="lastName"
+          name="lastName"
+          type="text"
+          value={lastName}
+          onChange={handleChange}
+        />
+        <label htmlFor="emailAddress">Email Address</label>
+        <input
+          id="emailAddress"
+          name="emailAddress"
+          type="email"
+          value={emailAddress}
+          onChange={handleChange}
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          id="password"
+          name="password"
+          type="password"
+          value={password}
+          onChange={handleChange}
+        />
+        <button class="button" type="submit">
+          Sign Up
+        </button>
+        <button
+          class="button button-secondary"
+          onclick="event.preventDefault(); location.href='index.html';"
+        >
+          Cancel
+        </button>
+      </form>
+      <p>
+        Already have a user account? Click here to{" "}
+        <Link to="sign-in.html">sign in</Link>!
+      </p>
+    </div>
+  );
 };
 
 export default UserSignUp;
